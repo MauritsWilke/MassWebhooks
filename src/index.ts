6ead3671c7226c126133dfa84d4e73abefc9e8e8
Maurits Wilke <mauritswilke@gmail.com>
2022-11-06 13:32:54 +0100

    feat: :sparkles: Styling now supports chaining

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { getAuthKey } from "./prompts/prompts.js";
 import { Styling } from "./styling.js";
 const Style = new Styling();
 
-console.log(Style.default("Add webhooks to all of your GitHub repositories!"));
-console.log(Style.default("To generate a token, go here: https://github.com/settings/tokens/new?scopes=repo"));
+Style.default("Add webhooks to all of your GitHub repositories!").log();
+Style.default("To generate a token, go here: https://github.com/settings/tokens/new?scopes=repo").log();
 const authKey = await getAuthKey();
 const octokit = new Octokit({
 	auth: authKey
--- src/styling.ts
+++ src/styling.ts
@@ -1,13 +1,36 @@
 import chalk from "chalk";
 
 export class Styling {
-	constructor(){};
+	private message:string;
+	constructor(){
+		this.message = "";
+	};
 
-	success = (message:string) => chalk.greenBright(`✓ ${message}`);
+	success = (message:string) => {
+		this.message = chalk.greenBright(`✓ ${message}`);
+		return this;
+	}
 
-	error = (message:string) => chalk.redBright(`! ${message}`);
+	error = (message:string) => {
+		this.message = chalk.redBright(`! ${message}`);
+		return this;
+	}
 
-	warning = (message:string) => chalk.yellowBright(`⚠ ${message}`);
+	warning = (message:string) => {
+		this.message = chalk.yellowBright(`⚠ ${message}`);
+		return this;
+	}
 
-	default = (message:string) => chalk.bold.blueBright(`> ${message}`);
+	default = (message:string) => {
+		this.message = chalk.bold.blueBright(`> ${message}`);
+		return this;
+	}
+
+	log(){
+		console.log(this.message);
+	}
+
+	toString(){
+		return this.message;
+	}
 }
