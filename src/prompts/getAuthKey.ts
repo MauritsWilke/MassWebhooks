503417115fbaa7795361b56b1438868016abf802
Maurits Wilke <mauritswilke@gmail.com>
2022-11-06 13:41:01 +0100

    fix: :bug: Fixed being able to leave key empty

--- src/prompts/getAuthKey.ts
+++ src/prompts/getAuthKey.ts
@@ -1,12 +1,21 @@
 import inquirer from "inquirer";
+import { Styling } from "../styling.js";
+const Style = new Styling();
 
 export async function getAuthKey(){
-	const { token }: { token: string } = await inquirer.prompt({
-		name: "token",
-		type: "password",
-		mask: "*",
-		message: "Please enter your token:"
-	});
+	let authKey:string|null = null;
 
-	return token;
+	while(!authKey){
+		const { token }: { token: string|null } = await inquirer.prompt({
+			name: "token",
+			type: "password",
+			mask: "*",
+			message: "Please enter your token:"
+		});
+
+		if(!token) Style.error("Please provide a token!").log();
+		else authKey = token;
+	}
+
+	return authKey;
 }
